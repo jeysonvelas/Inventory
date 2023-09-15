3802fd70161337ed298676625090e1a87997cc5d
jeysonvelas <jeysonvelasquez.v@gmail.com>
2023-09-14 23:12:59 -0500

    se modifican el archivo properties y se moficia el metodo post

--- src/main/java/com/inventory/net/inventory/controller/UserRestController.java
+++ src/main/java/com/inventory/net/inventory/controller/UserRestController.java
@@ -1,13 +1,10 @@
 package com.inventory.net.inventory.controller;
 
 
-import com.inventory.net.inventory.caseuse.CreateUser;
 import com.inventory.net.inventory.entity.Users;
 import com.inventory.net.inventory.service.UserService;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,16 +12,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 public class UserRestController {
-
-    @Autowired
-    private CreateUser createUser;
-
     @Autowired
     private UserService userService;
 
-    //private DeleteUser deleteUser;
-
-    //private UpdateUser updateUser;
 
 
     //Servicio Rest para obtener una Lista
@@ -37,9 +27,9 @@ public class UserRestController {
 
     //Servicio Rest para guardar un nuevo usuario
     @PostMapping("/")
-    ResponseEntity<Users> newUser(@RequestBody Users newUser){
-
-        return new ResponseEntity<>(createUser.save(newUser), HttpStatus.CREATED);
+    @ResponseStatus(HttpStatus.OK)
+    public void newUser(@RequestBody Users newUser){
+        userService.save(newUser);
 
     }
 
